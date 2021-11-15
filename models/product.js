10d042ae9dd776c23ec0a811f743310de6506782
Pulkit Bajaj <pulkit171112236@gmail.com>
2021-11-15 15:20:14 +0530

    added delete-product feature

--- models/product.js
+++ models/product.js
@@ -47,6 +47,15 @@ module.exports = class Product {
     })
   }
 
+  static deleteById(productId) {
+    getProductsFromFile((products) => {
+      const updatedProducts = products.filter((p) => p.id !== productId)
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) =>
+        console.log(err)
+      )
+    })
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb)
   }
