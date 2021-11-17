d431f8856f32932226f411ea677af1eeb8b3de0c
Pulkit Bajaj <pulkit171112236@gmail.com>
2021-11-17 16:50:02 +0530

    using sequelize for all

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,11 +33,13 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart().then(([rows]) => {
+  Cart.findAll({
+    include: [{ model: Product }],
+  }).then((cart) => {
     res.render('shop/cart', {
       pageTitle: 'Cart',
       path: '/cart',
-      products: rows,
+      cart: cart,
       totalPrice: 0,
     })
   })
@@ -45,17 +47,40 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId
-  Cart.addProduct(prodId).then((result) => {
-    console.log('result', result)
-    res.redirect('/cart')
-  })
+  Cart.findByPk(prodId)
+    .then((element) => {
+      if (element) {
+        return element.update({
+          qty: element.qty + 1,
+        })
+      } else {
+        return Cart.create({
+          product_id: prodId,
+          qty: 1,
+        })
+      }
+    })
+    .then(() => {
+      return res.redirect('/cart')
+    })
+    .catch((err) => {
+      console.log('__error_while_updating_cart__', err)
+    })
+  // Cart.addProduct(prodId).then((result) => {
+  // })
 }
 
 exports.postDeleteFromCart = (req, res, next) => {
   const productId = req.body.productId
-  Cart.deleteProduct(productId).then(() => {
-    res.redirect('/cart')
+  Cart.destroy({
+    where: { product_id: productId },
   })
+    .then(() => {
+      res.redirect('/cart')
+    })
+    .catch((err) => {
+      console.log('__error_while_deleting_from_cart__', err)
+    })
 }
 
 exports.getOrders = (req, res, next) => {
--- models/cart.js
+++ models/cart.js
@@ -8,49 +8,25 @@
 //   totalPrice
 // }
 
-const db = require('../util/database_mysql')
+const SequelizeClass = require('sequelize')
+const sequelize = require('../util/database')
 
-db.execute(
-  `CREATE TABLE IF NOT EXISTS cart (
-    product_id INTEGER NOT NULL,
-    qty INTEGER NOT NULL,
-    PRIMARY KEY (product_id),
-    FOREIGN KEY (product_id) REFERENCES products (id)
-      ON DELETE CASCADE
-      ON UPDATE NO ACTION
-  )
-  ENGINE=InnoDB`
-).catch((err) => {
-  console.log('err', err)
+const Product = require('./product')
+
+const Cart = sequelize.define('cart', {
+  product_id: {
+    type: SequelizeClass.INTEGER,
+    primaryKey: true,
+  },
+  qty: {
+    type: SequelizeClass.INTEGER,
+    allowNull: false,
+  },
 })
-module.exports = class Cart {
-  static async addProduct(id) {
-    return db
-      .execute('SELECT * FROM cart WHERE product_id = ?', [id])
-      .then(([rows]) => {
-        if (rows.length === 0) {
-          // add the element to cart with qty as 1
-          return db.execute(
-            'INSERT INTO cart (product_id, qty) VALUES (?, ?)',
-            [id, 1]
-          )
-        } else {
-          // update the existing element
-          return db.execute(
-            'UPDATE cart SET qty = qty+1 WHERE product_id = ?',
-            [id]
-          )
-        }
-      })
-  }
 
-  static deleteProduct(id) {
-    return db.execute('DELETE FROM cart WHERE product_id = ?', [id])
-  }
+Cart.belongsTo(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
 
-  static getCart() {
-    return db.execute(
-      'SELECT products.*, cart.qty FROM products JOIN cart ON products.id = cart.product_id'
-    )
-  }
-}
+module.exports = Cart
